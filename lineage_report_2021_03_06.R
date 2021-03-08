0380d6b05cc01b137946599de0131e9417d089c9
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2021-03-08 09:10:22 +0100

    daily update

--- lineage_report_2021_03_06.R
+++ lineage_report_2021_03_06.R
@@ -54,7 +54,7 @@ html <- paste(readLines("docs/index_source.html"), collapse = "\n")
 
 html <- gsub(pattern = "--DATE--", replacement = lineage_date, x = html)
 html <- gsub(pattern = "--NUMBER--", replacement = nrow(lineage), x = html)
-html <- gsub(pattern = "--VARIANTS--", replacement = nrow(lineage), x = html)
+html <- gsub(pattern = "--VARIANTS--", replacement = length(unique(lineage$Lineage)), x = html)
 
 warianty <- colnames(t_cou_lin)[-ncol(t_cou_lin)]
 warianty_list <- paste0(paste0('<a href="https://cov-lineages.org/lineages/lineage_',warianty,'.html">',warianty,'</a>'), collapse = ",\n")
@@ -125,77 +125,5 @@ pl4 <- ggplot(df3, aes(ymd(date), y=n, color = variant == "B.1.1.7", group = var
   theme(legend.position = "none")
 
 ggsave(plot = pl4, file="docs/images/liczba_warianty_2.svg", width=8, height=3)
-
-
-# -------
-# Nazwy wariantów
-
-
-
-
-
-# -------
-# Drzewo wariantów
-
-w_names <- unique(lineage$lineage)
-mat <- matrix(0, length(w_names), length(w_names))
-
-for (i in 1:length(w_names)) {
-  for (j in 1:length(w_names)) {
-    a <- paste(w_names[i], runif(1)*1000, runif(1)*1000, sep= ".")
-    b <- paste(w_names[j], runif(1)*1000, runif(1)*1000, sep= ".")
-    tmp <- strsplit(c(a, b), "\\.")
-    mat[i,j] <- mean(head(tmp[[1]], 5) != head(tmp[[2]], 5))
-  }
-}
-rownames(mat) <- w_names
-colnames(mat) <- w_names
-
-library("ggtree")
-require("ape") 
-hc <- hclust(as.dist(mat))
-plot(hclust(as.dist(mat)), horiz=TRUE)
-
-plot(as.phylo(hc), type = "unrooted", cex = 0.6)
-
-phy1 <- nj(mat)
-phy1 <- njs(mat)
-phy1
-ggtree(phy1, branch.length="none")
-ggtree(phy1, branch.length="none", layout="circular") + geom_tiplab()
-
-
-table(lineage$lineage)
-
-t_cou_lin <- table(lineage$date, lineage$lineage)
-t_cou_lin <- apply(t_cou_lin, 2, cumsum)
-
-H <- apply(t_cou_lin, 1, function(x) {
-  probs <- x/sum(x)
-  probs <- probs[probs > 0]
-  sum(-probs*log(probs))
-})
-
-N <- apply(t_cou_lin, 1, function(x) {
-  sum(x)
-})
-
-library(ggrepel)
-library(lubridate)
-df <- data.frame(cnt = names(H), H = H, N = N, date = ymd(names(H)))
-r1 <- ggplot(df[N>2,], aes(date, H, label = cnt)) + 
-  geom_step() + 
-  theme_light() + xlab("") + ylab("H entropy") +
-  ggtitle("GISAID: entropy of variants in Poland")
-
-r2 <- ggplot(lineage, aes(ymd(date))) + 
-  geom_histogram(bins = 100) + 
-  theme_light() + xlab("") + ylab("ncases") +
-  ggtitle("GISAID: number of sequences Poland")
-
-library(patchwork)
-r1 / r2 +
-  plot_layout(heights = c(2,1))
-
 
 
