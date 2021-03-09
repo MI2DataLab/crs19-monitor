d320c4d45d5b2db997fd83ead44810a99210f20c
Hanna Zdulska <zdula616@gmail.com>
2021-03-09 18:41:17 +0100

    wrap scrapping in python function

--- gisaid_scraping_fasta.py
+++ gisaid_scraping_fasta.py
@@ -8,53 +8,75 @@ from secret import elogin, epass  # file secret.py with credentials
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-url = "https://@epicov.org/epi3/"
+def get_number_of_files(dir):
+    if os.path.exists(dir):
+        n_files = len(os.listdir("./gisaid_data"))
+    else:
+        n_files = 0
+    return n_files
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.useWindow", False)
-profile.set_preference("browser.download.dir", os.getcwd() + "/gisaid_data")
-profile.set_preference(
-    "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream"
-)
+def scrap_fasta():
 
-options = webdriver.firefox.options.Options()
-# comment to allow firefox window
-options.add_argument("--headless")
+    url = "https://@epicov.org/epi3/"
+    download_dir = os.getcwd() + "/gisaid_data"
 
-driver = webdriver.Firefox(
-    executable_path="./geckodriver", firefox_profile=profile, options=options
-)
-driver.get(url)
-time.sleep(3)
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.useWindow", False)
+    profile.set_preference("browser.download.dir", download_dir)
+    profile.set_preference(
+        "browser.helperApps.neverAsk.saveToDisk", "application/octet-stream"
+    )
 
-# login
-driver.find_element_by_id("elogin").send_keys(elogin)
-driver.find_element_by_id("epassword").send_keys(epass)
-driver.find_element_by_class_name("form_button_submit").click()
-time.sleep(3)
+    options = webdriver.firefox.options.Options()
+    # comment to allow firefox window
+    options.add_argument("--headless")
 
-# navigate to search
-driver.find_elements_by_class_name("sys-actionbar-action")[1].click()
-time.sleep(3)
+    n_files_before = get_number_of_files(download_dir)
 
-# filter
-driver.find_element_by_id("ce_qpcb0m_a2_entry").send_keys("Europe / Poland")
-time.sleep(4)
+    driver = webdriver.Firefox(
+        executable_path="./geckodriver", firefox_profile=profile, options=options
+    )
+    driver.get(url)
+    time.sleep(3)
 
-# select all
-driver.find_elements_by_xpath("//input[starts-with(@type, 'checkbox')]")[5].click()
-time.sleep(10)
+    # login
+    driver.find_element_by_id("elogin").send_keys(elogin)
+    driver.find_element_by_id("epassword").send_keys(epass)
+    driver.find_element_by_class_name("form_button_submit").click()
+    time.sleep(3)
 
-# download
-driver.find_elements_by_class_name("sys-form-button")[4].click()
-time.sleep(3)
+    # navigate to search
+    driver.find_elements_by_class_name("sys-actionbar-action")[1].click()
+    time.sleep(3)
 
-# switch to frame
-iframe = driver.find_element_by_tag_name("iframe").get_attribute("id")
-driver.switch_to.frame(iframe)
-time.sleep(3)
-driver.find_elements_by_class_name("sys-event-hook.sys-form-button")[1].click()
+    # filter
+    driver.find_element_by_class_name("sys-event-hook.sys-fi-mark.yui-ac-input").send_keys("Europe / Poland")
+    time.sleep(4)
 
-time.sleep(5)
-driver.close()
+    driver.execute_script("document.getElementById('sys_curtain').remove()")
+
+    # select all
+    driver.find_elements_by_xpath("//input[starts-with(@type, 'checkbox')]")[5].click()
+    time.sleep(10)
+
+    # download
+    driver.find_elements_by_class_name("sys-form-button")[4].click()
+    time.sleep(3)
+
+    # switch to frame
+    iframe = driver.find_element_by_tag_name("iframe").get_attribute("id")
+    driver.switch_to.frame(iframe)
+    time.sleep(3)
+    driver.find_elements_by_class_name("sys-event-hook.sys-form-button")[1].click()
+
+    while get_number_of_files(download_dir) == n_files_before:
+        # sleep until file is downloaded
+        time.sleep(1)
+
+    time.sleep(1)
+    driver.close()
+
+    return download_dir
+
+scrap_fasta()
