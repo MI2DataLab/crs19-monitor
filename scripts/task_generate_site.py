bc0f32ba283c2c13694a3ae14b50dcb493bcc505
crs19monitor <crs19monitor@arena.mini.pw.edu.pl>
2021-03-13 11:14:57 +0000

    Update generate_site scripts

--- scripts/task_generate_site.py
+++ scripts/task_generate_site.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 repo_path = "/home/crs19monitor/crs19-monitor"
 data_dir = "/home/crs19monitor/data"
@@ -9,14 +10,16 @@ csv_path = data_dir + "/lineage_report.csv"
 tsv_path = data_dir + "/nextclade.tsv"
 meta_path = data_dir + "/metadata.csv"
 
-os.environ["LINEAGE_DATE"] = "2021/03/09"
+os.environ["LINEAGE_DATE"] = datetime.today().strftime('%Y/%m/%d')
 os.environ["LINEAGE_REPORT_PATH"] = csv_path
 os.environ["NEXTCLADE_REPORT_PATH"] = tsv_path
 os.environ["METADATA_REPORT_PATH"] = meta_path
 os.environ["OUTPUT_PATH"] = output_path
 
+remote =' origin' if not os.environ.get('DEV') else 'dev'
+
 out = os.system('Rscript ' + exec_path)
 if out == 0:
-    os.system('cd ' + output_path + ' && git add -A && git commit -m "update" && git push -f -u origin gh-pages')
+    os.system('cd ' + output_path + ' && git add -A && git commit -m "update" && git push -f -u ' + remote + ' gh-pages')
 else:
     pass
