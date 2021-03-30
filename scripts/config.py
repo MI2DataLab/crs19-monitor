99f95cae7ce0e95ce75270df53d61a2ede74d499
Piotr Piatyszek <piotrp@wektor.xyz>
2021-03-30 10:08:24 +0000

    Add pushing site to multiple remotes

--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,8 @@ repo_path = "/crs19/production/repo"
 data_dir = "/crs19/production/data"
 site_dist = "/crs19/production/site_dist"
 backup_dir = "/crs19/production/backups"
+# List of remotes (name,ssh key,cname)
+remotes = [('crs19', '/home/crs19monitor/.ssh/id_rsa', 'monitor.crs19.pl'),('mi2ai', '/home/crs19monitor/.ssh/id_rsa.mi2ai', 'monitor.mi2.ai')]
 
 gisaid_fasta_dir = data_dir + '/gisaid_fasta'
 clades_output_dir = data_dir + '/clades'
--- scripts/task_generate_site.py
+++ scripts/task_generate_site.py
@@ -2,7 +2,7 @@ import os
 import glob
 import sys
 from datetime import datetime
-from config import repo_path, site_dist, pango_merged_file, clades_merged_file, mutation_merged_file, db_path
+from config import repo_path, site_dist, pango_merged_file, clades_merged_file, mutation_merged_file, db_path, remotes
 
 work_dir = repo_path + '/generate_site'
 exec_path = "script.R"
@@ -16,11 +16,14 @@ os.environ["DB_PATH"] = db_path
 os.environ["MAIN_REGION"] = main_region
 os.environ["OUTPUT_PATH"] = site_dist
 
-remote =' origin'
+out = 0
+if not os.environ.get('NOT_BUILD'):
+    out = os.system('cd ' + work_dir + ' && Rscript ' + exec_path)
 
-out = os.system('cd ' + work_dir + ' && Rscript ' + exec_path)
 if out == 0 and not os.environ.get('NOT_PUSH'):
-    out = os.system('cd ' + site_dist + ' && git add -A && git commit -m "update" && git push -f -u ' + remote + ' gh-pages')
-    sys.exit(out >> 8)
-else:
-    sys.exit(out >> 8)
+    for remote in remotes:
+        os.environ['GIT_SSH_COMMAND'] = 'ssh -i ' + remote[1] + ' -o IdentitiesOnly=yes'
+        out = os.system('cd ' + site_dist + ' && echo "' + remote[2] + '" > CNAME && git add -A && git commit -m "update" && git push -f -u ' + remote[0] + ' gh-pages')
+        if out != 0:
+            sys.exit(out >> 8)
+sys.exit(out >> 8)
