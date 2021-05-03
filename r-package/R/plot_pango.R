2fe1a8b30e8e4b86f5deebe96cc560457b1f06eb
Hubert Baniecki <hbaniecki@gmail.com>
2021-05-03 15:10:42 +0200

    lower padding, make y axis in cumulative plots log10

--- r-package/R/plot_pango.R
+++ r-package/R/plot_pango.R
@@ -82,7 +82,10 @@ plot_pango_cumulative <- function(df,
     scale_x_date("", date_breaks = "2 weeks", date_labels = "%m/%d",
                  limits = c(ymd(lineage_date) %m-% months(no_months_plots_long), ymd(lineage_date))) +
     theme_minimal(base_family = "Arial") +
-    scale_y_continuous("", expand = c(0, 0)) +
+    scale_y_log10(
+      breaks = scales::trans_breaks("log10", function(x) 10^x),
+      labels = scales::trans_format("log10", scales::math_format(10^.x)),
+      name = "", expand = c(0, 0)) +
     ggtitle(title) +
     theme(legend.position = "none")
 
