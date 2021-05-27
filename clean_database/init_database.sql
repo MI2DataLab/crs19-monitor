90f6a065ec3937b5bdb0a945b4bd3f7adaec28a0
Piotr Piatyszek <piotrp@wektor.xyz>
2021-05-27 20:12:18 +0000

    update cleaning script

--- clean_database/init_database.sql
+++ clean_database/init_database.sql
@@ -1,5 +1,35 @@
+DROP TABLE IF EXISTS substitutions_bridge;
 DROP TABLE IF EXISTS substitutions;
 DROP TABLE IF EXISTS sequences;
+DROP TABLE IF EXISTS dates;
+DROP TABLE IF EXISTS pango;
+DROP TABLE IF EXISTS clade;
+
+CREATE TABLE dates (
+  date TEXT PRIMARY KEY NOT NULL,
+  year INT NOT NULL,
+  month INT NOT NULL,
+  day INT NOT NULL,
+  week INT NOT NULL,
+  weekday INT NOT NULL,
+  week_start TEXT NOT NULL,
+  month_start TEXT NOT NULL,
+  is_weekend INT NOT NULL
+);
+
+CREATE TABLE clade (
+  clade TEXT PRIMARY KEY NOT NULL,
+  color TEXT NULL,
+  is_alarm INT NOT NULL,
+  name TEXT NULL
+);
+
+CREATE TABLE pango (
+  pango TEXT PRIMARY KEY NOT NULL,
+  color TEXT NULL,
+  is_alarm INT NOT NULL,
+  name TEXT NULL
+);
 
 CREATE TABLE sequences (
   accession_id TEXT PRIMARY KEY NOT NULL,
@@ -14,19 +44,34 @@ CREATE TABLE sequences (
   submitting_lab TEXT NULL,
   min_age INT NULL,
   max_age INT NULL,
+  sex TEXT NULL,
   our_clade TEXT NULL,
   our_pango TEXT NULL,
   gisaid_clade TEXT NULL,
   gisaid_variant TEXT NULL,
-  gisaid_pango TEXT NULL
+  gisaid_pango TEXT NULL,
+  FOREIGN KEY (collection_date) REFERENCES dates(date),
+  FOREIGN KEY (submission_date) REFERENCES dates(date)
+  FOREIGN KEY (our_pango) REFERENCES pango(pango),
+  FOREIGN KEY (gisaid_pango) REFERENCES pango(pango),
+  FOREIGN KEY (our_clade) REFERENCES clade(clade)
 );
 CREATE INDEX location_index ON sequences(continent, country);
-CREATE INDEX pango_index ON sequences(our_pango);
-CREATE INDEX clade_index ON sequences(our_clade);
+CREATE INDEX our_pango_index ON sequences(our_pango);
+CREATE INDEX gisaid_pango_index ON sequences(gisaid_pango);
+CREATE INDEX our_clade_index ON sequences(our_clade);
 
 CREATE TABLE substitutions (
-  accession_id TEXT NOT NULL,
+  substitution_id INT PRIMARY KEY NOT NULL,
   substitution TEXT NOT NULL,
-  PRIMARY KEY (accession_id, substitution),
-  FOREIGN KEY (accession_id) REFERENCES sequences(accession_id)
+  source TEXT NOT NULL
+);
+CREATE INDEX substitutions_source_index ON substitutions(source);
+
+CREATE TABLE substitutions_bridge (
+  accession_id TEXT NOT NULL,
+  substitution_id INT NOT NULL,
+  PRIMARY KEY (accession_id, substitution_id),
+  FOREIGN KEY (accession_id) REFERENCES sequences(accession_id),
+  FOREIGN KEY (substitution_id) REFERENCES substitutions(substitution_id)
 );
--- clean_database/script.py
+++ clean_database/script.py
@@ -1,10 +1,13 @@
 import sqlite3
 import os
+import re
 
 import pandas as pd
+import numpy as np
 from unidecode import unidecode
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
+from tqdm import tqdm
 
 
 def init_db(db_path):
@@ -45,6 +48,10 @@ def simplify_location(location):
     return None if location is None else unidecode(location).lower().replace('-', ' ').replace('_', ' ')
 
 
+minimal_date = datetime.strptime('2019-12-01', '%Y-%m-%d')
+today_date = datetime.today()
+
+
 def clean_date(date):
     # Fast path
     if date is None:
@@ -56,9 +63,73 @@ def clean_date(date):
         return date
     # Slow path
     try:
-        return datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d')
+        parsed = datetime.strptime(date, '%Y-%m-%d')
+        if minimal_date <= parsed <= today_date:
+            return parsed.strftime('%Y-%m-%d')
     except:
         return None
+
+
+def clean_age(age):
+    if age is None:
+        return (None, None)
+    age = str(age)
+    age = age.replace(" ", "")
+    age = age.lower()
+
+    def single_sanity_check(x):
+        return x if 0 <= x <= 120 else None
+
+    def sanity_check(lower, upper):
+        return (lower, upper) if (0 <= lower <= 120 and 0 <= upper <= 120 and lower <= upper) else (None, None)
+
+    range_ = re.findall(r"\d+-\d+", age)
+    if range_:
+        bounds = range_[0].split("-")
+        return sanity_check(int(bounds[0]), int(bounds[1]))
+    lower = re.findall(r">\d+", age)
+    if lower:
+        low = single_sanity_check(int(lower[0][1:]))
+        return (low, None)
+    lower = re.findall(r"\d+\+", age)
+    if lower:
+        low = single_sanity_check(int(lower[0][:-1]))
+        return (low, None)
+    upper = re.findall(r"<\d+", age)
+    if upper:
+        up = single_sanity_check(int(upper[0][1:]))
+        return (None, up)
+    range_ = re.findall(r"\d+to\d+", age)
+    if range_:
+        bounds = range_[0].split("to")
+        return sanity_check(int(bounds[0]), int(bounds[1]))
+    months = re.findall(r"\d+month", age)
+    if months:
+        age_ = single_sanity_check(int(months[0][:-5]) / 12)
+        return (age_, age_)
+    range_ = re.findall(r"\d+unknown", age)
+    if range_:
+        decimal = range_[0][:-7]
+        return sanity_check(int(decimal + "0"), int(decimal + "9"))
+    years = re.findall(r"\d+year", age)
+    if years:
+        years = years[0][:-4]
+        return sanity_check(int(years), int(years))
+    else:
+        return (None, None)
+
+
+def clean_sex(sex):
+    if sex is None:
+        return None
+    sex = sex.lower()
+    if sex in ["female", "moteris", "femmina", "f"]:
+        return "Female"
+    if sex in ["male", "maschio", "m"]:
+        return "Male"
+    else:
+        return None
+
 
 def update_locations(raw_db, loc_db):
     """
@@ -99,14 +170,130 @@ def update_locations(raw_db, loc_db):
         con.commit()
 
 
-def load(raw_db, loc_db, clean_db, pango_path, clades_path):
+def load_dates(clean_db, dates):
+    """
+    Fill dates table
+    """
+    print('Filling dates table')
+    fmt = '%Y-%m-%d'
+    dates = [datetime.strptime(x, fmt) for x in dates]
+    with sqlite3.connect(clean_db) as con:
+        cur = con.cursor()
+        for d in dates:
+            fields = ['date', 'year', 'month', 'day', 'week', 'week_start', 'month_start', 'is_weekend', 'weekday']
+            values = [d.strftime(fmt), d.year, d.month, d.day, d.isocalendar()[1], (d - timedelta(days=d.weekday())).strftime(fmt), datetime(d.year, d.month, 1).strftime(fmt), d.weekday() >= 6, d.weekday() + 1]
+            cur.execute('INSERT INTO dates (' + ','.join(fields) + ') VALUES (' + ','.join((['?'] * len(fields))) + ')', values)
+        con.commit()
+
+
+def load_clade(clean_db, clade_config_path, unique_clade):
+    """
+    Fill clade table
+    """
+    print('Filling clade table')
+    config = pd.read_csv(clade_config_path, sep='\t')
+    others = [str(x) for x in unique_clade if x not in list(config['clade'])]
+    with sqlite3.connect(clean_db) as con:
+        config.to_sql('clade', con, if_exists='append', index=None)
+        cur = con.cursor()
+        for other in others:
+            cur.execute('INSERT INTO clade (clade, color, is_alarm, name) VALUES (?, ?, ?, ?)', (other, None, 0, ''))
+        con.commit()
+
+
+def load_pango(clean_db, pango_config_path, unique_pango):
+    """
+    Fill pango table
+    """
+    print('Filling pango table')
+    config = pd.read_csv(pango_config_path, sep='\t')
+    others = [str(x) for x in unique_pango if x not in list(config['pango'])]
+    with sqlite3.connect(clean_db) as con:
+        config.to_sql('pango', con, if_exists='append', index=None)
+        cur = con.cursor()
+        for other in others:
+            cur.execute('INSERT INTO pango (pango, color, is_alarm, name) VALUES (?, ?, ?, ?)', (other, None, 0, ''))
+        con.commit()
+
+
+def load_substitutions(clean_db, raw_db, df):
+    """
+    Fill substitutions and substitutions_bridge tables
+    """
+    def load_part(con, frames, ids_gen, source):
+        cur = con.cursor()
+        for sub_id, sub_name in ids_gen.get_update():
+            cur.execute('INSERT INTO substitutions (substitution_id, substitution, source) VALUES (?, ?, ?)', (sub_id, sub_name, source))
+        con.commit()
+
+        merged = pd.concat(frames)
+        merged.to_sql('substitutions_bridge', con, if_exists='append', index=None)
+
+    class ids_generator:
+        def __init__(self, start_id=0):
+            self.last_new_id = start_id
+            self.ids_dict = {}
+            self.last_loaded_id = start_id
+        def get_id(self, name):
+            id_ = self.ids_dict.get(name)
+            if id_ is None:
+                id_ = self.last_new_id = self.last_new_id + 1
+                self.ids_dict[name] = id_
+            return id_
+        def get_update(self):
+            ids = [(id_, name) for name, id_ in self.ids_dict.items() if id_ > self.last_loaded_id]
+            self.last_loaded_id = self.last_new_id
+            return ids
+
+    print('Filling substitutions and substitutions_bridge table')
+    with sqlite3.connect(clean_db) as con:
+        frames = []
+        id_gen = ids_generator()
+        for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+            # Each row contain substitutions as strings seperated by comma
+            subs = np.unique((row['aaSubstitutions'] if type(row['aaSubstitutions']) is str else '').split(','))
+            if len(subs) == 1 and subs[0] == '':
+                continue
+            # Generate ids for given substitutions
+            subs_ids = [id_gen.get_id(sub) for sub in subs]
+            frames.append(pd.DataFrame({'accession_id': row['accession_id'], 'substitution_id': subs_ids}))
+            # Load part to db
+            if len(frames) == 50000:
+                load_part(con, frames, id_gen, 'our')
+                frames = []
+        if len(frames) > 0:
+            load_part(con, frames, id_gen, 'our')
+            frames = []
+        # We want to keep separate rows for different sources
+        id_gen = ids_generator(id_gen.last_new_id)
+        with sqlite3.connect(raw_db) as raw_con:
+            raw_cur = raw_con.cursor()
+            raw_cur.execute('SELECT accession_id, substitutions FROM metadata WHERE substitutions IS NOT NULL')
+            for accession_id, substitutions_str in tqdm(raw_cur.fetchall()):
+                # Each row contain substitutions as strings seperated by comma
+                subs = np.unique((substitutions_str if type(substitutions_str) is str else '').split(','))
+                if len(subs) == 1 and subs[0] == '':
+                    continue
+                # Generate ids for given substitutions
+                subs_ids = [id_gen.get_id(sub) for sub in subs]
+                frames.append(pd.DataFrame({'accession_id': accession_id, 'substitution_id': subs_ids}))
+                # Load part to db
+                if len(frames) == 50000:
+                    load_part(con, frames, id_gen, 'gisaid')
+                    frames = []
+            if len(frames) > 0:
+                load_part(con, frames, id_gen, 'gisaid')
+                frames = []
+
+
+def load(raw_db, loc_db, clean_db, pango_path, clades_path, clade_config_path, pango_config_path):
     """
     Cleans data and loads to new database
     """
     with sqlite3.connect(raw_db) as con:
         columns = [
             'accession_id', 'location', 'passage', 'submission_date', 'collection_date', 'host',
-            'gisaid_pango', 'clade as gisaid_clade', 'variant as gisaid_variant'
+            'gisaid_pango', 'clade as gisaid_clade', 'variant as gisaid_variant', 'sex', 'age'
         ]
         raw = pd.read_sql('select ' + ','.join(columns) + ' from metadata', con)
     with sqlite3.connect(loc_db) as con:
@@ -126,32 +313,58 @@ def load(raw_db, loc_db, clean_db, pango_path, clades_path):
     raw['collection_date'] = [clean_date(x) for x in raw['collection_date']]
     raw['submission_date'] = [clean_date(x) for x in raw['submission_date']]
 
+    # Get unique dates
+    unique_collection_date = np.unique([x for x in raw['collection_date'] if x is not None]).tolist()
+    unique_submission_date = np.unique([x for x in raw['submission_date'] if x is not None]).tolist()
+    unique_dates = np.unique(unique_collection_date + unique_submission_date)
+    load_dates(clean_db, unique_dates)
+
+    # Clean sex
+    print('Cleaning sex')
+    raw['sex'] = [clean_sex(x) for x in raw['sex']]
+
+    # Clean age
+    print('Cleaning age')
+    ages = [clean_age(x) for x in raw['age']]
+    raw['min_age'] = [x[0] for x in ages]
+    raw['max_age'] = [x[1] for x in ages]
+    raw = raw.drop(columns=['age'])
+
     # Clades
     print('Loading clade')
     clades = pd.read_csv(clades_path, sep='\t')
     clades['accession_id'] = [x.split('|')[1] for x in clades['seqName']]
-    clades = clades[['accession_id', 'clade']].rename(columns={'clade': 'our_clade'})
+    clades['clade'] = [str(x) if x is not np.nan and x is not None else np.nan for x in clades['clade']]
     clades.drop_duplicates(['accession_id'], keep='first', inplace=True)
+    # substitutions will be used later
+    substitutions = clades[['accession_id', 'aaSubstitutions']]
+    clades = clades[['accession_id', 'clade']].rename(columns={'clade': 'our_clade'})
     raw = pd.merge(raw, clades, how='left', on='accession_id')
+
+    # Get unique clade
+    unique_clade = np.unique([str(x) for x in clades['our_clade'] if x is not None and x is not np.nan])
+    load_clade(clean_db, clade_config_path, unique_clade)
 
     # Pango
     print('Loading pango')
     pango = pd.read_csv(pango_path)
     pango['accession_id'] = [x.split('|')[1] for x in pango['taxon']]
+    pango['lineage'] = [str(x) if x is not np.nan and x is not None and x != 'None' else np.nan for x in pango['lineage']]
     pango = pango[['accession_id', 'lineage']].rename(columns={'lineage': 'our_pango'})
     pango.drop_duplicates(['accession_id'], keep='first', inplace=True)
     raw = pd.merge(raw, pango, how='left', on='accession_id')
 
+    # Get unique clade
+    unique_pango = np.unique([str(x) for x in pango['our_pango'] if x is not None and x is not np.nan])
+    load_pango(clean_db, pango_config_path, unique_pango)
+
     # Load
-    print('Saving to clean database')
+    print('Saving sequences to clean database')
     with sqlite3.connect(clean_db) as con:
-        #cur = con.cursor()
-        #columns = list(raw.columns)
-        #columns_values = [[row[c] for c in columns] for index, row in raw.iterrows()]
-        # Columns names are safe
-        #cur.executemany('INSERT INTO sequences (' + ','.join(columns) + ') VALUES (' + ','.join(['?'] * len(columns)) + ')', columns_values)
-        #con.commit()
         raw.to_sql('sequences', con, if_exists='append', index=None)
+
+    # Substitutions
+    load_substitutions(clean_db, raw_db, substitutions)
 
 
 if __name__ == "__main__":
@@ -160,10 +373,12 @@ if __name__ == "__main__":
     CLEAN_DB_PATH = os.environ.get("CLEAN_DB_PATH")
     PANGO_PATH = os.environ.get('PANGO_PATH')
     CLADES_PATH = os.environ.get('CLADES_PATH')
+    CLADE_CONFIG_PATH = os.environ.get('CLADE_CONFIG_PATH')
+    PANGO_CONFIG_PATH = os.environ.get('PANGO_CONFIG_PATH')
     print('Initializing database')
     init_db(CLEAN_DB_PATH)
     print('Creating locations database')
     init_locations_db(LOCATIONS_DB_PATH)
     update_locations(RAW_DB_PATH, LOCATIONS_DB_PATH)
     print('Cleaning data')
-    load(RAW_DB_PATH, LOCATIONS_DB_PATH, CLEAN_DB_PATH, PANGO_PATH, CLADES_PATH)
+    load(RAW_DB_PATH, LOCATIONS_DB_PATH, CLEAN_DB_PATH, PANGO_PATH, CLADES_PATH, CLADE_CONFIG_PATH, PANGO_CONFIG_PATH)
