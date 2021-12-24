2df0f7fe9c5bdc4a59577643370d05db829b5a62
Piotr Piatyszek <piotrp@wektor.xyz>
2021-12-24 01:10:34 +0000

    Update backup script

--- scripts/task_backup.py
+++ scripts/task_backup.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import sys
 import glob
 import config
-from config import data_dir, backup_dir, backups_number, site_dist, clean_db_path
+from config import data_dir, backup_dir, backups_number, site_dist, clean_db_path, excluded_from_backup
 
 for d in [data_dir, backup_dir, site_dist]:
     if not os.path.exists(d):
@@ -12,7 +12,7 @@ for d in [data_dir, backup_dir, site_dist]:
 
 filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.tar.gz'
 to_precompress = [config.clean_db_path, config.db_path, config.pango_merged_file, config.clades_merged_file]
-to_exclude = [config.clades_output_dir, config.pango_output_dir, '.git']
+to_exclude = [config.clades_output_dir, config.pango_output_dir, '.git'] + excluded_from_backup
 
 # Precompress single files
 for path in to_precompress:
